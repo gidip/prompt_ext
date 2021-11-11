874a4bbfcf1733b8012cc0cba5f3f999593bd4f8
Hakan <hakan_ak@hotmail.co.uk>
2021-11-11 16:07:27 +0000

    Add files via upload

---
+++ prompt_ext.py
@@ -0,0 +1,9 @@
+
+#Enter any number to multiply with any time table
+
+a=int(input("Enter table number "))
+b=int(input("Enter the number to which table is to printed "))
+i=1
+while i<=b:
+    print(a,"x",i,"=",a*i)
+    i=i+1
